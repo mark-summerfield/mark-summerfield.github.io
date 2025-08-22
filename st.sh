2a23ec5f93c963c2faa1ac9996eddc2b8a5f1fad
mark-summerfield <mark@qtrac.eu>
2025-08-22 16:21:23 +0100

    m

--- st.sh
+++ st.sh
@@ -1,5 +1,6 @@
 if [ $# -ne 0 ]; then
-    sitecheck .
+    sitecheck . |grep -v \
+	img.tag.has.nonexistent.src.https://static.fsf.org/nosvn/esp/logos/patent-free.svg
 else
     echo "use v to validate html pages"
 fi
